ea4fcd24156a6d88eb05ed5c0acc81d9d9c2f5b6
M-Kub <mkubesch@gmail.com>
2020-10-31 15:24:38 +0100

    Jinja-Temp-Imp

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='index')
+    path('home/', views.home, name='index'),
+    path('', views.blogList, name='blogList'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home/index.html')
+
+
+def blogList(request):
+    return render(request, 'home/blogList.html')
