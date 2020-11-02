fe0fd115ba1ec44532379a71df8b14b586e9eb7a
M-Kub <mkubesch@gmail.com>
2020-11-02 10:50:45 +0100

    Added blog functionallity incl. images

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('home/', views.home, name='index'),
-    path('', views.blogList, name='blogList'),
-]
+    path('', views.post_list, name='post_list'),
+    path('post/(<int:pk>)/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
+    path('post/(<int:pk>)/edit/', views.post_edit, name='post_edit'),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/views.py
+++ home/views.py
@@ -1,10 +1,53 @@
-"""Imports"""
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
+from django.utils import timezone
+from .models import Post
+from .forms import PostForm
 
 
 def home(request):
     return render(request, 'home/index.html')
 
 
-def blogList(request):
-    return render(request, 'home/blogList.html')
+def post_list(request):
+    """REQUESTS POSTS"""
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'home/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    """REQUESTS FULL POST"""
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'home/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    """WRITE NEW POST"""
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'home/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    """EDIT POST"""
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'home/post_edit.html', {'form': form})
