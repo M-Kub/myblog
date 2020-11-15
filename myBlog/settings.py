4249a0b337e89a798d1b54f50cbff9217e6e9532
M-Kub <mkubesch@gmail.com>
2020-11-15 11:46:26 +0000

    setting changes media

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -132,5 +132,5 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
