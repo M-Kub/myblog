4648a340c27915a97939d9f01790ee200853fe5b
M-Kub <mkubesch@gmail.com>
2023-10-06 12:49:32 +0200

    FormField Size - removed Crispy

--- home/forms.py
+++ home/forms.py
@@ -6,4 +6,7 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
+        widgets = {
+          'text': forms.Textarea(attrs={'rows':45, 'cols':85}),
+        }
         fields = ('title', 'text', 'document', 'image')
