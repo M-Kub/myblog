c65a3a111fafb4b7306353cf85240891f153a1a8
M-Kub <mkubesch@gmail.com>
2024-08-19 18:09:42 +0200

    restyling

--- home/urls.py
+++ home/urls.py
@@ -14,4 +14,7 @@ urlpatterns = [
     path('impressum/', views.impressum, name='impressum')
 
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
