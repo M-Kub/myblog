06fcbf12fed6c79db885e8a74e3cd734a36de781
M-Kub <mkubesch@gmail.com>
2025-06-27 19:36:27 +0200

    Added setting to surpress devServer warning

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -4,6 +4,9 @@ import json
 from django.core.exceptions import ImproperlyConfigured
 
 
+# Surpress deploymentserver warning.
+os.environ["DJANGO_RUNSERVER_HIDE_WARNING"] = "true"
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
