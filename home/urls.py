d7f4921a3b825bb092c7ed08cb0ec0bf97603129
M-Kub <mkubesch@gmail.com>
2025-06-27 16:24:55 +0200

    Fixed setting debug and home/urls

--- home/urls.py
+++ home/urls.py
@@ -7,14 +7,13 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('home/', views.home, name='index'),
     path('', views.post_list, name='post_list'),
-    path('post/(<int:pk>)/', views.post_detail, name='post_detail'),
-    path('post/new', views.post_new, name='post_new'),
-    path('post/(<int:pk>)/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('about/', views.about, name='about'),
     path('impressum/', views.impressum, name='impressum')
-
-
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- myBlog/settings.py
+++ myBlog/settings.py
@@ -22,25 +22,20 @@ def get_secret(setting, secrets=secrets):
 
 DEBUG = get_secret('DEBUG')
 
-if DEBUG:
-    ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'mikub.me', 'www.mikub.me']
-else:
-    ALLOWED_HOSTS = ['212.227.206.87', '2001:8d8:1801:263::', 'www.mikub.me', 'mikub.me']
 # My Changes #
 
 # Get Postgres-DB Pass
 with open(os.path.join(BASE_DIR, 'myBlog/stuff.json')) as secrets_file:
     secrets = json.load(secrets_file)
 
-if DEBUG:
-    SECRET_KEY = '00n*n10o*z4!v2%rx9_i303tj%jxp0bxll&v#_&uozarlj)+sr'
-else:
-    SECRET_KEY = get_secret('DJANGO_SECRET_KEY')
-
 # ServerSec
 if DEBUG:
+    SECRET_KEY = '00n*n10o*z4!v2%rx9_i303tj%jxp0bxll&v#_&uozarlj)+sr'
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'mikub.me', 'www.mikub.me']
     SECURE_SSL_REDIRECT = False
 else:
+    SECRET_KEY = get_secret('DJANGO_SECRET_KEY')
+    ALLOWED_HOSTS = ['212.227.206.87', '2001:8d8:1801:263::', 'www.mikub.me', 'mikub.me']
     SECURE_SSL_REDIRECT = True
 
 #################################################
